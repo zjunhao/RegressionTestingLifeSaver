ba0cd42fad647ccbd3572ff03404b0876a9d07b2
Junhao Zhang <junhao.zhang@hyland.com>
2021-04-30 16:21:16 -0400

    added testplan finish page

---
+++ RegressionTestingLifeSaver/Pages/TestPlanFinishPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegressionTestingLifeSaver.Pages
+{
+    /// <summary>
+    /// Interaction logic for TestPlanFinishPage.xaml
+    /// </summary>
+    public partial class TestPlanFinishPage : Page
+    {
+        public TestPlanFinishPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
